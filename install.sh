1bf74d7eeeb461b753246c9c54a2d131444d6d6f
Abhishek Kumar <abhishekkumar@Rohans-MacBook-Air.local>
2021-05-24 19:03:15 +0530

    minor

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ pkg up
 
 pkg install python
 
+pkg install git
+
 pkg install termux-api
 
 termux-setup-storage
@@ -9,7 +11,7 @@ termux-setup-storage
 cd ~/storage/shared
 
 pip install wheel
-pkg install libjpeg-turbo
+pkg install libjpeg-turbo libpng
 LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
 
 pkg install clang
